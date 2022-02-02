9985a830926b9e73aafcfe1773c56c2d6fd8e089
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2022-02-01 23:00:14 -0300

    feat(PageWithAppBar): improve layout

--- src/components/templates/PageWithAppBar/style.js
+++ src/components/templates/PageWithAppBar/style.js
@@ -1,2 +1,11 @@
 import styled from "styled-components";
-export const PageBox = styled.div``;
+export const PageBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  main {
+    flex-grow: 1;
+    overflow: auto;
+    padding: ${({theme})=>theme.spacing.md};
+  }
+`;
