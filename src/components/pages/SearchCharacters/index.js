8e0d4f057a61ba41f1be749d1fac57faa6abb6d5
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2022-02-02 00:45:31 -0300

    feat(SearchCharactersPage): implement search input

--- src/components/pages/SearchCharacters/index.js
+++ src/components/pages/SearchCharacters/index.js
@@ -1,7 +1,12 @@
+import SearchInput from "../../organisms/SearchInput";
 import PageWithAppBar from "../../templates/PageWithAppBar";
 
 function SearchCharactersPage() {
-  return <PageWithAppBar title="AnimeVita">hello/</PageWithAppBar>;
+  return (
+    <PageWithAppBar title="AnimeVita">
+      <SearchInput onSearch={value => console.log("submit", value)} />
+    </PageWithAppBar>
+  );
 }
 
 export default SearchCharactersPage;
