4d3a80354a0ec2b170a0df2d4b5f670faf406cc6
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2022-02-02 20:50:28 -0300

    feat(theme): add neutral pallette

--- src/themes/default.js
+++ src/themes/default.js
@@ -16,6 +16,13 @@ export const theme = {
       light: "#ACACAC",
       medium: "#4B4B4B",
       dark: "#1F1F1F"
+    },
+    neutral: {
+      lightest: '#EBEBEB',
+      light: '#D6D6D6',
+      medium: '#ACACAC',
+      dark: '#4B4B4B',
+      darkest: '#1F1F1F'
     }
   },
   typography: {
