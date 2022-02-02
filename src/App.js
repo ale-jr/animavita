006e247893e13bd36d55c0aef9e376df5090853c
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2022-02-01 22:55:44 -0300

    feat: create character search page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import { ThemeProvider } from "styled-components";
+import SearchCharactersPage from "./components/pages/SearchCharacters";
+import { GlobalStyle } from "./globalStyles";
 import { theme } from "./themes/default";
 function App() {
-  return <ThemeProvider theme={theme}>hello world</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <SearchCharactersPage />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/components/pages/SearchCharacters/index.js
@@ -0,0 +1,7 @@
+import PageWithAppBar from "../../templates/PageWithAppBar";
+
+function SearchCharactersPage() {
+  return <PageWithAppBar title="AnimeVita">hello/</PageWithAppBar>;
+}
+
+export default SearchCharactersPage;
