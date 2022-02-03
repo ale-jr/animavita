5d74c2e9d9367c926da7182380dd02635c487572
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2022-02-02 23:43:14 -0300

    feat(Avatar): add border to improve contrast

--- src/components/atoms/Avatar/style.js
+++ src/components/atoms/Avatar/style.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
 export const Avatar = styled.img`
-border-radius: 50%;
-width: 100px;
-height: 100px;
-object-fit: cover;
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  border: 1px solid ${({ theme }) => theme.colors.neutral.medium};
 `;
